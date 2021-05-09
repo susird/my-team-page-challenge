f92a3cdacf51bf7e11a8be784d3bca02399cd683
susird <susir95@gmail.com>
2021-05-09 00:27:56 -0500

    mock data for my team page

---
+++ data.js
@@ -0,0 +1,33 @@
+export const data = {
+  "header": {
+    "title-team": "The creative crew",
+    "description-team-title": "Who we are",
+    "description-team-text": "We are team of creatively diverse. driven. innovative individuals working in various locations from the world."
+  },
+
+  "team-member-section": {
+    "team-member-card": {
+      "image": "/home/susanar/Documentos/Programacion_2021_1/Layouts/my-team-page/photo4.png",
+      "name-title": "Skylah Lu",
+      "charge-text": "UX DESIGNER"
+    }
+  },
+
+  "footer": {
+    "socialMedia":
+      [
+        {
+          "href": "https://instagram.com/ve5pula",
+          "image": "https://www.flaticon.es/svg/static/icons/svg/725/725339.svg"
+        },
+        {
+          "href": "https://twitter.com/ve5pula",
+          "image": "https://www.flaticon.es/svg/static/icons/svg/725/725372.svg"
+        },
+        {
+          "href": "https://facebook.com",
+          "image": "https://www.flaticon.es/svg/static/icons/svg/725/725350.svg"
+        }
+      ]
+  }
+}
