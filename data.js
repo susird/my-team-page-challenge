10e33b87cdcb7a396ecdd5ceb3af2f8c0b28c472
susird <susir95@gmail.com>
2021-05-12 22:26:56 -0500

    adding images and modifiying data

--- data.js
+++ data.js
@@ -2,14 +2,39 @@ export const data = {
   "header": {
     "title-team": "The creative crew",
     "description-team-title": "Who we are",
-    "description-team-text": "We are team of creatively diverse. driven. innovative individuals working in various locations from the world."
+    "description-team-text": "We are a team of creatively diverse-driven innovative individuals working in various locations from the world."
   },
   "team-member-section": {
     "team-member-card": [
       {
+        "image": "/home/susanar/Documentos/Programacion_2021_1/Layouts/my-team-page/photo1.png",
+        "name-title": "Bill Mahoney",
+        "charge-text": "PRODUCT OWNER"
+      },
+      {
+        "image": "/home/susanar/Documentos/Programacion_2021_1/Layouts/my-team-page/photo2.png",
+        "name-title": "Saba Cabrera",
+        "charge-text": "ART DIRECTOR"
+      },
+      {
+        "image": "/home/susanar/Documentos/Programacion_2021_1/Layouts/my-team-page/photo3.png",
+        "name-title": "Shae Le",
+        "charge-text": "TECH LEAD"
+      },
+      {
         "image": "/home/susanar/Documentos/Programacion_2021_1/Layouts/my-team-page/photo4.png",
         "name-title": "Skylah Lu",
         "charge-text": "UX DESIGNER"
+      },
+      {
+        "image": "/home/susanar/Documentos/Programacion_2021_1/Layouts/my-team-page/photo5.png",
+        "name-title": "Griff Richards",
+        "charge-text": "DEVELOPER"
+      },
+      {
+        "image": "/home/susanar/Documentos/Programacion_2021_1/Layouts/my-team-page/photo6.png",
+        "name-title": "Stan John",
+        "charge-text": "DEVELOPER"
       }
     ]
   },
