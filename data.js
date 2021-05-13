fac9e7db1f33c8680a4f2469703ec34d0a3cac00
susird <susir95@gmail.com>
2021-05-12 21:57:05 -0500

    insert aria for images, deleting blank spaces

--- data.js
+++ data.js
@@ -4,7 +4,6 @@ export const data = {
     "description-team-title": "Who we are",
     "description-team-text": "We are team of creatively diverse. driven. innovative individuals working in various locations from the world."
   },
-
   "team-member-section": {
     "team-member-card": [
       {
@@ -14,21 +13,18 @@ export const data = {
       }
     ]
   },
-
   "footer": {
-    "socialMedia":
+    "social-media":
       [
         {
           "href": "https://instagram.com/ve5pula",
-          "image": "https://www.flaticon.es/svg/static/icons/svg/725/725339.svg"
+          "image": "https://www.flaticon.es/svg/static/icons/svg/725/725339.svg",
+          "alt": "Instagram Image"
         },
         {
           "href": "https://twitter.com/ve5pula",
-          "image": "https://www.flaticon.es/svg/static/icons/svg/725/725372.svg"
-        },
-        {
-          "href": "https://facebook.com",
-          "image": "https://www.flaticon.es/svg/static/icons/svg/725/725350.svg"
+          "image": "https://www.flaticon.es/svg/static/icons/svg/725/725372.svg",
+          "alt": "Twitter Image"
         }
       ]
   }
