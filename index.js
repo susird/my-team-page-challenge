e9d175570ef988301c6f841fa0788508ab1fc6f2
susird <susir95@gmail.com>
2021-06-10 17:50:21 -0500

    adding styles, fixing index.js file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import { createHeader } from './Components/Header.js';
+import { createHeader } from './Components/Header';
 
 const rootElement = document.getElementById('root');
+const headerElement = createHeader()
 
 rootElement.appendChild(headerElement);
